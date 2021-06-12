a6803508ac19c45c48b2bb1cc2067ca36da6a655
rehankhan21 <narutop224@gmail.com>
2021-06-12 17:16:37 -0400

    what is CRM?

---
+++ Salesforce.js
@@ -0,0 +1,19 @@
+/*
+    What is it?
+
+    Salesforce is a customer succes platform, designed to help you sell, service, amrket, analyze. and
+    connect with you customers.
+
+    Store data securely in the cloud.
+
+    
+
+    What is CRM?
+
+    CRM - Costumer Relationship Manager.
+    This technology allows you to manage relationships with your customers and prospects and track data related to all
+    of your interactions. It also helps teams collaborate, both internally and externally, gatther insights from social
+    media, track important metrics, and communicate via email, phone, social, other channels.
+
+    
+*/
