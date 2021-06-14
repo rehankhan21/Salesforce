c663fedde469704aaf13d6f84271f5cf85f82895
rehankhan21 <narutop224@gmail.com>
2021-06-14 00:28:59 -0400

    Created Lightning web component hello world

--- Salesforce.js
+++ Salesforce.js
@@ -15,5 +15,11 @@
     of your interactions. It also helps teams collaborate, both internally and externally, gatther insights from social
     media, track important metrics, and communicate via email, phone, social, other channels.
 
-    
+    In Salesforce, all this information is stored securely in the cloud. 
+
+    How Salesforce Organizes Your Data
+    - Salesforce oraganizes your data into OBJECTS AND RECORDS. Objects are Tabs or Tables, Records are rows on a
+    table like on a spreadsheet.
+
+
 */
